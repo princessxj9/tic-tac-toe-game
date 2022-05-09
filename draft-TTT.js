f670bf657b3c84075bba47ea7d5f2803a3d2f498
princessxj9 <johnsonprincess25@gmail.com>
2022-05-09 18:02:46 -0400

    commit

--- draft-TTT.js
+++ draft-TTT.js
@@ -1,3 +1,45 @@
-const board;
-const playerX ='X'
-const playerO = 'O'
+const cell = Array.from(document.getElementsByClassName('cell'));
+console.log(cell);
+
+const playerX ='X';
+const playerO = 'O';
+var currPlayer = playerX; 
+
+let winCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [2,5,8],
+    [1,4,7],
+    [0,3,6],
+]
+
+cell.forEach(function(cell){
+    cell.addEventListener('click', function(){
+        cell.innerText = currPlayer;
+
+        if (currPlayer == playerX) {
+            currPlayer = playerO;
+        }
+        else {
+            currPlayer = playerX;
+        }
+    })
+})
+
+
+
+
+
+
+/*
+let gameOver = false;
+window.onload = function() {
+    startGame();
+}
+
+function clearBoard(){
+    document.getElementsByClassName('cell').reset();
+}*/
